6c1a9822f6e1d4890998c0b57555ebc8d37959d2
Eric-Bwr <boewer2003@gmail.com>
2021-06-27 22:08:06 +0200

    New

--- Assets/Shader.glsl
+++ Assets/Shader.glsl
@@ -5,34 +5,40 @@ void main() {
     gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
 }
 
-#geometry
-#version 330 core
+    #geometry
+    #version 330 core
 
-#define POINTS 1
+#define VERT 400
 
 layout (points) in;
-layout (line_strip, max_vertices = 20) out;
+layout (line_strip, max_vertices = 800) out;
 
-uniform vec2[1] coords;
+uniform vec2 mouse;
+uniform float time;
 
 void drawLine(int index){
-    gl_Position = vec4(coords[index].x, coords[index].y, 0.0, 1.0);
+    gl_Position = vec4(cos(index) * cos(time), sin(index) * cos(time), 0.0, 1.0);
     EmitVertex();
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_Position = vec4(sin(index) * tan(time), cos(index) * sin(time), 0.0, 1.0);
     EmitVertex();
 }
 
 void main(){
-    for(int i = 0; i < POINTS; i++)
+    for(int i = 0; i < VERT; i++){
         drawLine(i);
+    }
+   //gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+   //EmitVertex();
+   //gl_Position = vec4(mouse.x / 1920 * 1.25 * 1, (1 - (mouse.y / 1080) * 1.25) - 1, 0.0, 1.0);
+   //EmitVertex();
     EndPrimitive();
 }
 
-#fragment
-#version 330 core
+    #fragment
+    #version 330 core
 
 out vec4 FragColor;
 
 void main() {
-    FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- Triangulation/Main.cpp
+++ Triangulation/Main.cpp
@@ -2,85 +2,80 @@
 #include <Shader.h>
 #include <Buffer.h>
 #include <iostream>
+#include <cmath>
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
     HWND p = FindWindowEx(hwnd, NULL, reinterpret_cast<LPCSTR>("SHELLDLL_DefView"), NULL);
     HWND *ret = (HWND *) lParam;
-
-    if (p) {
-// Gets the WorkerW Window after the current one.
+    if (p)
         *ret = FindWindowEx(NULL, hwnd, reinterpret_cast<LPCSTR>("WorkerW"), NULL);
-    }
-
     return true;
 }
 
 HWND GetWallpaperWindow() {
-    // Fetch the Progman window
     HWND progman = FindWindow(reinterpret_cast<LPCSTR>("ProgMan"), NULL);
-    // Send 0x052C to Progman. This message directs Progman to spawn a
-    // WorkerW behind the desktop icons. If it is already there, nothing
-    // happens.
     SendMessageTimeout(progman, 0x052C, 0, 0, SMTO_NORMAL, 1000, nullptr);
-    // We enumerate all Windows, until we find one, that has the SHELLDLL_DefView
-    // as a child.
-    // If we found that window, we take its next sibling and assign it to workerw.
     HWND wallpaper_hwnd = nullptr;
     EnumWindows(EnumWindowsProc, (LPARAM) &wallpaper_hwnd);
     return wallpaper_hwnd;
 }
 
+typedef const char* (WINAPI* PFNWGLGETEXTENSIONSSTRINGEXTPROC) (void);
+typedef BOOL(WINAPI* PFNWGLSWAPINTERVALEXTPROC) (int interval);
+
 int main() {
-    // Get the HWND for the desktop background window
     HWND window = GetWallpaperWindow();
     HDC hdc = GetDC(window);
     PIXELFORMATDESCRIPTOR pfd = {
                     sizeof(PIXELFORMATDESCRIPTOR),
                     1,
                     PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    // Flags
-                    PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
-                    32,                   // Colordepth of the framebuffer.
+                    PFD_TYPE_RGBA,
+                    24,
                     0, 0, 0, 0, 0, 0,
                     0,
                     0,
                     0,
                     0, 0, 0, 0,
-                    24,                   // Number of bits for the depthbuffer
-                    8,                    // Number of bits for the stencilbuffer
-                    0,                    // Number of Aux buffers in the framebuffer.
+                    0,
+                    0,
+                    0,
                     PFD_MAIN_PLANE,
                     0,
                     0, 0, 0
             };
-
-    // Try setting the pixel format, this will be =0 if this fails
     int format = ChoosePixelFormat(hdc, &pfd);
-
     SetPixelFormat(hdc, format, &pfd);
-
-    // Create the OpenGL context
     if (HGLRC hglrc = wglCreateContext(hdc)) {
         wglMakeCurrent(hdc, hglrc);
-        // OpenGL 1.1 is supported by default without needing to explicitly load functions
         if (!gladLoadGL()) {
             std::cout << "Failed to init GLAD\n";
         }
+        if (strstr(((PFNWGLGETEXTENSIONSSTRINGEXTPROC) wglGetProcAddress("wglGetExtensionsStringEXT"))(), "WGL_EXT_swap_control") != NULL)
+            ((PFNWGLSWAPINTERVALEXTPROC) wglGetProcAddress("wglSwapIntervalEXT"))(1);
         Shader shader("../Assets/Shader.glsl");
         shader.bind();
         std::cout << shader.getErrorMessage();
         glLineWidth(1);
+        glPointSize(10);
         glEnable(GL_LINE_SMOOTH);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        while (true) {
+        POINT mouse;
+        float time = 0;
+        while (!(GetKeyState(VK_ESCAPE) & 0x8000)) {
+            time += 0.001;
+            GetCursorPos(&mouse);
             glClear(GL_COLOR_BUFFER_BIT);
-            glClearColor(0.5, 1.0, 0.5, 1.0);
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            shader.setUniform1f("time", time);
+           // shader.setUniform2f("mouse", mouse.x, mouse.y);
 //           shader.setUniform2fv("coords", MAX_POINTS * sizeof(float) * 2, reinterpret_cast<const float *>(coords));
             glDrawArrays(GL_POINTS, 0, 1);
+            Sleep(1);
             SwapBuffers(hdc);
         }
-        wglMakeCurrent(NULL, NULL);
+        wglMakeCurrent(nullptr, nullptr);
         wglDeleteContext(hglrc);
         ReleaseDC(window, hdc);
     } else {
